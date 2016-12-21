e8f836055145b69256170e271841ead332e3a50f
Carly <cj.langlois.cl@gmail.com>
2016-12-20 20:25:52 -0600

    full calendar displays

--- js/home-script.js
+++ js/home-script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    //brand scroll change
     $(window).scroll(function() {
         if (($(document).scrollTop() > 100) | ($(window).width() < 800)){
             $(".navbar").addClass("minimized");
@@ -8,6 +9,18 @@ $(document).ready(function(){
             $(".navbar").removeClass("minimized");
             $("#brand-img").attr("src", "images/img-name.jpg");
         }
+    });
+
+    //date click
+    $(".date").click(function(){
+        var date_val = $(this).text();
+        console.log(date_val);
+    });
+
+    //change month
+    $(".next").click(function(){
+        $("#month").text("February");
+        $("#month span").text("2017");
     });
 
 });
