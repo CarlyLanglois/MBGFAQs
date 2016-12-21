6845503a4969dd68a83954ccee9008934ad8c9c3
Carly <cj.langlois.cl@gmail.com>
2016-12-20 21:08:41 -0600

    events added

--- js/home-script.js
+++ js/home-script.js
@@ -11,6 +11,25 @@ $(document).ready(function(){
         }
     });
 
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+        events:[
+            {
+                title: "Speaker Series: History of the Climatron",
+                start: "2017-01-17T11:00:00",
+                end: "2017-01-17T12:00:00",
+                allDay : false
+            },
+            {
+                title: "Orchid Show Preview",
+                start: "2017-02-03T17:30:00",
+                end: "2017-02-03T20:30:00",
+                allDay: false
+            }
+        ]
+    });
+
+    /*
     //date click
     $(".date").click(function(){
         var date_val = $(this).text();
@@ -20,7 +39,10 @@ $(document).ready(function(){
     //change month
     $(".next").click(function(){
         $("#month").text("February");
-        $("#month span").text("2017");
     });
+
+    //months
+    var months = {"January" , "February", "March", }
+    */
 
 });
